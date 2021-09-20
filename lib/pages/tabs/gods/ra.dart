c42be1becf449a4cc4d254ab738454b3fbf25e7f
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2021-09-21 02:02:14 +0300

    add a custom dialog for myths

--- lib/pages/tabs/gods/ra.dart
+++ lib/pages/tabs/gods/ra.dart
@@ -6,6 +6,72 @@ class RaGod extends StatefulWidget {
   State<RaGod> createState() => _RaGodState();
 }
 
+class CustomDialog extends StatelessWidget {
+    final String title, description;
+    late String buttonText;
+    late Image image;
+    CustomDialog({required this.title, required this.description});
+    @override
+    Widget build(BuildContext context) {
+        return Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16)
+            ),
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            child: dialogContent(context)
+        );
+    }
+    Widget dialogContent(BuildContext context) {
+        return Stack(
+            children: <Widget> [
+                Container(
+                    padding: EdgeInsets.only(top: 100, bottom: 14, left: 10, right: 10),
+                    margin: EdgeInsets.only(top:16),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(17),
+                        boxShadow: [
+                            BoxShadow( color: Colors.black26, blurRadius: 10,
+                                offset: Offset(0,10),)
+                        ]
+                    ),
+                    child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                            Text(title, style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.w700
+                            )),
+                            SizedBox(height: 20),
+                            Expanded(
+                                child: SingleChildScrollView(
+                                    child: Text(description, style: TextStyle( fontSize: 14)),
+                                ),
+                            ),
+                            Align(
+                                alignment: Alignment.bottomRight,
+                                child: TextButton(
+                                    onPressed: (){ Navigator.pop(context); },
+                                    child: Text('Nice'),
+                                ),
+                            ),
+                        ],
+                    ),
+                ),
+                Positioned(
+                    top: 0,
+                    left: 16,
+                    right: 16,
+                    child: CircleAvatar(backgroundColor: Colors.blue, radius: 50,
+                        backgroundImage: AssetImage('assets/gods/god1.png')
+                    )
+                ),
+            ],
+        );
+    }
+}
+
 class _RaGodState extends State<RaGod> {
   @override
   Widget build(BuildContext context) {
@@ -17,7 +83,10 @@ class _RaGodState extends State<RaGod> {
         padding: const EdgeInsets.symmetric(vertical: 20.0),
         width: MediaQuery.of(context).size.width,
         child: ElevatedButton(
-          onPressed: () => {},
+          onPressed: () => {
+              showDialog(context: context, builder: (context) => 
+                CustomDialog(title: 'Ra\'s myth', description: 'lorem ipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumloremm iipsumlorem iipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem psumlorem ipsumlorem iipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumlorempsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum'))
+          },
           child: const Text("I WANNA READ THE MYTH",
               style: TextStyle(color: Colors.white)),
         ));
