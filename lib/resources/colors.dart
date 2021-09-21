fd0d5e82ec4f42b925762bc38c499291190ca3ab
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2021-09-21 13:00:14 +0300

    add base colors

---
+++ lib/resources/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CColors {
+    // 60 or primary colors
+    static Color mainbg = const Color(0xff2D2D2F);
+    static Color secondbg = const Color(0xffCD9C2E);
+    // 30 or secondary colors
+    static Color gold = const Color(0xffCD9C2E);
+    static Color blue = const Color(0xffAAC1A7);
+    // 10 or accent colors
+    static Color brown = const Color(0xff6D583B);
+    static Color lace = const Color(0xffF6F0E2);
+}
+
