cfc7ec390112ae6caa895d05bdc02026ac489668
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2021-09-23 12:23:28 +0300

    add a splash screen and modify colors of nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:egyptianmythology/pages/home.dart';
 import 'package:egyptianmythology/pages/intro.dart';
 import 'package:egyptianmythology/pages/notsofast.dart';
+import 'package:egyptianmythology/resources/colors.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: CColors.mainbg, // Navigation bar color
+    systemNavigationBarDividerColor: CColors.mainbg, //Navigation bar divider color
+  ));
+
   var currentDate = DateTime.now();
   var releaseDate = DateTime(2021, 9, 27, 13, 35);
   late String firstRoute = '/loading';
